070345b1ee0fad8270c23f02aedb7d698fb0756d
Vaibhav Singh <vaibhavsingh1720@gmail.com>
2021-01-12 15:57:21 +0530

    Login and logout features added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  # devise_for :users
+  devise_for :users
+
+  devise_scope :user do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+
   resources :doubts
   # get 'home/index'
   get 'home/about'
